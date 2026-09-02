a9ffe69a48fbb5307c64afc33effcded1a8649b3
Kevin <kxveein@gmail.com>
2026-09-02 11:59:08 +0530

    Update Chatbot.jsx
    i change server url

--- src/component/Chatbot.jsx
+++ src/component/Chatbot.jsx
@@ -5,7 +5,7 @@ import './Chatbot.css'
 import Chatboticon from './Chatboticon'
 
 // 👉 Change this to your actual backend endpoint.
-const API_URL = "http://localhost:5000/api/chat";
+const API_URL = "http://localhost:10000/api/chat";
 
 const Chatpopuscreen = () => {
   const [chathistory, sethistory] = useState([]);
